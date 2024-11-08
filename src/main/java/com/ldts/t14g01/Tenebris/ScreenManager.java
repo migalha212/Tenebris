a06938cf79cae6fb417bb39236b97ebcd33a5491
Claudio Meireles <claudiomeireles555@gmail.com>
2024-11-08 18:27:02 +0000

    Create ScreenManager class
    Add functionality to create screens with a fixed aspect ratio,
    that occupy the same screen size but with different number
    of cells.

---
+++ src/main/java/com/ldts/t14g01/Tenebris/ScreenManager.java
@@ -0,0 +1,45 @@
+package com.ldts.t14g01.Tenebris;
+
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.swing.SwingTerminalFontConfiguration;
+
+import java.awt.*;
+import java.io.IOException;
+
+public class ScreenManager {
+    private static final double SCREEN_OCCUPANCY = 0.7;
+    private static final int BASE_WIDTH = 16;
+    private static final int BASE_HEIGHT = 5;
+
+    public static final int MAIN_MENU = 5;
+    public static final int ARENA = 12;
+
+    public static Screen createScreen(int multiplier) throws IOException {
+        // Calculate Terminal Cell Count
+        final int numberCols = BASE_WIDTH * multiplier;
+        final int numberRows = BASE_HEIGHT * multiplier;
+
+        // Get screen dimensions
+        int screenHeight = (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+
+        // Calculate terminal window dimensions (80% of screen)
+        int windowHeight = (int) (screenHeight * SCREEN_OCCUPANCY);
+
+        // Calculate font size and height
+        int fontSize = windowHeight / numberRows;
+
+        // Set up Lanterna terminal with calculated font size
+        Screen screen = new DefaultTerminalFactory()
+                .setInitialTerminalSize(new TerminalSize(numberCols, numberRows))
+                .setTerminalEmulatorFontConfiguration(
+                        SwingTerminalFontConfiguration.newInstance(
+                                new Font("Monospaced", Font.BOLD, fontSize)
+                        )
+                ).createScreen();
+
+        screen.doResizeIfNecessary();
+        return screen;
+    }
+}
--- src/main/java/com/ldts/t14g01/Tenebris/Tenebris.java
+++ src/main/java/com/ldts/t14g01/Tenebris/Tenebris.java
@@ -1,7 +1,21 @@
 package com.ldts.t14g01.Tenebris;
 
+import com.googlecode.lanterna.screen.Screen;
+
+import java.io.IOException;
+
 public class Tenebris {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    Screen screen;
+
+    public static void main(String[] args) throws IOException {
+        Tenebris tenebris = new Tenebris();
+        tenebris.run();
+    }
+
+    Tenebris() throws IOException {
+        this.screen = ScreenManager.createScreen(ScreenManager.MAIN_MENU);
+    }
+
+    public void run() {
     }
 }
