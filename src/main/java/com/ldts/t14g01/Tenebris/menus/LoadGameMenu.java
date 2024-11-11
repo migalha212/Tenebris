5cc43e64fcc771b3efe77ce54d1726e068034448
Claudio Meireles <claudiomeireles555@gmail.com>
2024-11-11 18:00:48 +0000

    Add All Menu's as empty Classes

---
+++ src/main/java/com/ldts/t14g01/Tenebris/menus/LoadGameMenu.java
@@ -0,0 +1,11 @@
+package com.ldts.t14g01.Tenebris.menus;
+
+import com.ldts.t14g01.Tenebris.State;
+import com.ldts.t14g01.Tenebris.screen.ScreenGetter;
+
+import java.io.IOException;
+
+public class LoadGameMenu implements Menu{
+    public void run(ScreenGetter screenGetter, State state) throws IOException, InterruptedException {
+    }
+}
