571419de4bebe81e54aedeec1fcd38072ec17bca
DinisBSilva <up202306207@up.pt>
2024-11-12 15:26:52 +0000

    Finalized Credits Menu

--- src/main/java/com/ldts/t14g01/Tenebris/menus/CreditsMenu.java
+++ src/main/java/com/ldts/t14g01/Tenebris/menus/CreditsMenu.java
@@ -73,18 +73,82 @@ public class CreditsMenu implements Menu {
         // Get TextGraphics
         TextGraphics textGraphics = screen.newTextGraphics();
 
-        // Get center (y axis) position
+        // Get center position
         int centerX = screen.getTerminalSize().getColumns() / 2;
         int centerY = screen.getTerminalSize().getRows() / 2;
 
+        // X axis Offset
+        int offsetX = 4;
+
         // Draw Title
+        List<String> title = new ArrayList<>();
+        title.add(CreditsMenu.name);
+        title.add("───────────");
+
+        for (int i = 0; i < title.size(); i++) {
+            textGraphics
+                    .setForegroundColor(TextColor.Factory.fromString("#DAA520"))
+                    .putString(
+                            offsetX,
+                            centerY - 8 + i,
+                            title.get(i)
+                    );
+        }
+
+        //Draw UC Info
+        List<String> ucInfo = new ArrayList<>();
+        ucInfo.add("2024/2025");
+        ucInfo.add("FEUP L.EIC014 - LDTS");
+
+        for (int i = 0; i < title.size(); i++) {
+            textGraphics
+                    .setForegroundColor(TextColor.ANSI.WHITE)
+                    .putString(
+                            screen.getTerminalSize().getColumns() - offsetX - ucInfo.get(i).length(),
+                            centerY - 8 + i,
+                            ucInfo.get(i)
+                    );
+        }
+
+        //Draw Authors' Name
+        List<String> nameAuthor = new ArrayList<>();
+        nameAuthor.add("T14G01:");
+        nameAuthor.add("Claúdio Meireles");
+        nameAuthor.add("Dinis Silva");
+        nameAuthor.add("Miguel Pereira");
+
         textGraphics
                 .setForegroundColor(TextColor.ANSI.WHITE)
                 .putString(
-                        centerX - CreditsMenu.name.length()/2,
-                        centerY - 8,
-                        CreditsMenu.name
+                        offsetX + 2,
+                        centerY - 3,
+                        nameAuthor.get(0)
                 );
+
+        for (int i = 1; i < nameAuthor.size(); i++) {
+            textGraphics
+                    .setForegroundColor(TextColor.ANSI.WHITE)
+                    .putString(
+                            offsetX + 3,
+                            centerY + i - 2,
+                            nameAuthor.get(i)
+                    );
+        }
+
+        //Draw Regent and Supervisor Names
+        List<String> names = new ArrayList<>();
+        names.add("Rui Maranhão");
+        names.add("Juvenaldo Carvalho");
+
+        for (int i = 0; i < names.size(); i++){
+            textGraphics
+                    .setForegroundColor(TextColor.ANSI.WHITE)
+                    .putString(
+                            screen.getTerminalSize().getColumns() - offsetX - names.get(i).length(),
+                            centerY + i - 1,
+                            names.get(i)
+                    );
+        }
 
         //Draw Back Button
         String backText = "Back";
