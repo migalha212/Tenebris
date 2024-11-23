93c8dbfc6fd1ec73aef4888f1caf090f2d6e9bbe
Claudio Meireles <claudiomeireles555@gmail.com>
2024-11-23 17:02:58 +0000

    Update SaveData class to meet new specification

---
+++ src/main/java/com/ldts/t14g01/Tenebris/SaveData.java
@@ -0,0 +1,44 @@
+package com.ldts.t14g01.Tenebris;
+
+import com.ldts.t14g01.Tenebris.utils.Difficulty;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SaveData {
+    private Difficulty difficulty;
+    private int level;
+
+    public SaveData(Difficulty difficulty) {
+        this.difficulty = difficulty;
+        this.level = 1;
+    }
+
+    private SaveData(Difficulty difficulty, int level) {
+        this.difficulty = difficulty;
+        this.level = level;
+    }
+
+    public void save() {
+        // ToDo : Implement save function
+    }
+
+    public List<SaveData> loadSaves() {
+        List<SaveData> saves = new ArrayList<>();
+
+        // ToDo : Implement load function
+        return saves;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void increaseLevel() {
+        this.level++;
+    }
+}
