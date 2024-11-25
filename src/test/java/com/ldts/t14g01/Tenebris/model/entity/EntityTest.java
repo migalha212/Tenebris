96cba5ff3a67670da967c834fd555f67742a77e5
migalha212 <migalha240@gmail.com>
2024-11-25 01:58:58 +0000

    added Tests for the currently implemented methods inside the Models of our elements

---
+++ src/test/java/com/ldts/t14g01/Tenebris/model/entity/EntityTest.java
@@ -0,0 +1,36 @@
+package com.ldts.t14g01.Tenebris.model.entity;
+
+import com.ldts.t14g01.Tenebris.model.arena.entity.Entity;
+import com.ldts.t14g01.Tenebris.utils.Vector2D;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class EntityTest {
+    private Entity entity;
+
+    @BeforeEach
+    void setUp(){
+        Vector2D position = new Vector2D(1,1);
+        int size = 1;
+        int hp = 10;
+        int maxVelocity = 5;
+        entity = new Entity(position,size,hp,maxVelocity);
+    }
+
+    @Test
+    void isAliveTest(){
+        Assertions.assertTrue(entity.isAlive());
+    }
+    @Test
+    void isAliveTest2(){
+        entity.takeDamage(9);
+        Assertions.assertTrue(entity.isAlive());
+    }
+    @Test
+    void isNotAliveTest(){
+        entity.takeDamage(20);
+        Assertions.assertFalse(entity.isAlive());
+    }
+
+}
---
+++ src/test/java/com/ldts/t14g01/Tenebris/model/staticelement/BreakableWallTest.java
@@ -0,0 +1,32 @@
+package com.ldts.t14g01.Tenebris.model.staticelement;
+
+import com.ldts.t14g01.Tenebris.model.arena.staticelement.BreakableWall;
+import com.ldts.t14g01.Tenebris.utils.Vector2D;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BreakableWallTest {
+    private BreakableWall breakableWall;
+
+    @BeforeEach
+    void setUp(){
+        Vector2D position = new Vector2D(1,1);
+        breakableWall = new BreakableWall(position,1,10);
+    }
+
+    @Test
+    void isAliveTest(){
+        Assertions.assertTrue(breakableWall.isAlive());
+    }
+    @Test
+    void isAliveTest2(){
+        breakableWall.takeDamage(9);
+        Assertions.assertTrue(breakableWall.isAlive());
+    }
+    @Test
+    void isNotAliveTest(){
+        breakableWall.takeDamage(20);
+        Assertions.assertFalse(breakableWall.isAlive());
+    }
+}
