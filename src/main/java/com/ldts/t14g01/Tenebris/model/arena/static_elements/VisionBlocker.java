5a4d49adcf16d47a7d3389653f5790bbdd533d40
DinisBSilva <up202306207@up.pt>
2024-12-18 21:39:45 +0000

    Level 6 Completed

--- src/main/java/com/ldts/t14g01/Tenebris/model/arena/static_elements/VisionBlocker.java
+++ src/main/java/com/ldts/t14g01/Tenebris/model/arena/static_elements/VisionBlocker.java
@@ -2,6 +2,7 @@ package com.ldts.t14g01.Tenebris.model.arena.static_elements;
 
 import com.ldts.t14g01.Tenebris.model.arena.GameElement;
 import com.ldts.t14g01.Tenebris.model.arena._commands.Command;
+import com.ldts.t14g01.Tenebris.model.arena.interfaces.AbsorbsProjectiles;
 import com.ldts.t14g01.Tenebris.model.arena.interfaces.BlocksVision;
 import com.ldts.t14g01.Tenebris.utils.HitBoX;
 import com.ldts.t14g01.Tenebris.utils.Vector2D;
@@ -10,7 +11,7 @@ import com.ldts.t14g01.Tenebris.view.arena.staticelement.VisionBlockerView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class VisionBlocker extends GameElement implements BlocksVision {
+public class VisionBlocker extends GameElement implements BlocksVision, AbsorbsProjectiles {
     private static final HitBoX hitBoX = new HitBoX(new Vector2D(-9, -9), new Vector2D(17, 17));
 
     public VisionBlocker(Vector2D position) {
