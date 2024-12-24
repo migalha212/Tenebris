cbb190f7f29e5732b24a3ec75f089078fae8c696
Miguel Pereira <migalha240@gmail.com>
2024-12-24 01:38:18 +0000

    Added Tests for Game Over Menu Controller

---
+++ src/test/java/com/ldts/t14g01/Tenebris/controller/menu/GameOverMenuControllerTest.java
@@ -0,0 +1,78 @@
+package com.ldts.t14g01.Tenebris.controller.menu;
+
+import com.ldts.t14g01.Tenebris.Tenebris;
+import com.ldts.t14g01.Tenebris.gui.Action;
+import com.ldts.t14g01.Tenebris.gui.GUI;
+import com.ldts.t14g01.Tenebris.model.menu.GameOverMenu;
+import com.ldts.t14g01.Tenebris.sound.SoundManager;
+import com.ldts.t14g01.Tenebris.state.MenuState;
+import com.ldts.t14g01.Tenebris.state.StateChanger;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+public class GameOverMenuControllerTest {
+    private GameOverMenuController controller;
+    private GameOverMenu model;
+    private StateChanger stateChanger;
+    private SoundManager soundManager;
+    private GUI gui;
+
+    @BeforeEach
+    void setUp() {
+        this.model = new GameOverMenu();
+        this.controller = new GameOverMenuController(model);
+        this.stateChanger = Mockito.mock(StateChanger.class);
+        this.soundManager = Mockito.mock(SoundManager.class);
+        this.gui = Mockito.mock(GUI.class);
+    }
+
+    @Test
+    void tickEscTest() throws Exception {
+        try (MockedStatic<GUI> mockedGUI = Mockito.mockStatic(GUI.class); MockedStatic<SoundManager> mockedSoundManager = Mockito.mockStatic(SoundManager.class)) {
+            mockedGUI.when(GUI::getGUI).thenReturn(this.gui);
+            mockedSoundManager.when(SoundManager::getInstance).thenReturn(this.soundManager);
+
+            Mockito.when(this.gui.getAction()).thenReturn(Action.ESC);
+
+            // Frame delay for actions is 15, so the game needs to tick 15 times to have an action go off
+            for(int i = 0; i < 15; i++) this.controller.tick(stateChanger, Tenebris.getInstance());
+
+            Mockito.verify(this.soundManager, Mockito.times(1)).playSFX(SoundManager.SFX.MENU_GO_BACK);
+            Mockito.verify(this.stateChanger, Mockito.times(1)).setState(Mockito.any(MenuState.class));
+        }
+    }
+
+    @Test
+    void tickEXECTest() throws Exception {
+        try (MockedStatic<GUI> mockedGUI = Mockito.mockStatic(GUI.class); MockedStatic<SoundManager> mockedSoundManager = Mockito.mockStatic(SoundManager.class)) {
+            mockedGUI.when(GUI::getGUI).thenReturn(this.gui);
+            mockedSoundManager.when(SoundManager::getInstance).thenReturn(this.soundManager);
+
+            Mockito.when(this.gui.getAction()).thenReturn(Action.EXEC);
+
+            // Frame delay for actions is 15, so the game needs to tick 15 times to have an action go off
+            for(int i = 0; i < 15; i++) this.controller.tick(stateChanger, Tenebris.getInstance());
+
+            Mockito.verify(this.soundManager, Mockito.times(1)).playSFX(SoundManager.SFX.MENU_GO_BACK);
+            Mockito.verify(this.stateChanger, Mockito.times(1)).setState(Mockito.any(MenuState.class));
+        }
+    }
+
+
+    @Test
+    void tickQuitTest() throws Exception {
+        try (MockedStatic<GUI> mockedGUI = Mockito.mockStatic(GUI.class); MockedStatic<SoundManager> mockedSoundManager = Mockito.mockStatic(SoundManager.class)) {
+            mockedGUI.when(GUI::getGUI).thenReturn(this.gui);
+            mockedSoundManager.when(SoundManager::getInstance).thenReturn(this.soundManager);
+
+            Mockito.when(this.gui.getAction()).thenReturn(Action.QUIT);
+
+            // Frame delay for actions is 15, so the game needs to tick 15 times to have an action go off
+            for(int i = 0; i < 15; i++) this.controller.tick(stateChanger, Tenebris.getInstance());
+
+            Mockito.verify(this.stateChanger, Mockito.times(1)).setState(null);
+        }
+    }
+}
